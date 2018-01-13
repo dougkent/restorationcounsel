769c7752298d947a47c0e488f33d11d78ec87511
Doug Kent <doug.kent88@gmail.com>
2018-01-12 19:14:56 -0600

    Initial commit

---
+++ restorationcounsel.com/js/app.js
@@ -0,0 +1,24 @@
+﻿var app = angular.module("rcApp", ["ngRoute"]);
+
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "js/home.component.html"
+        })
+        .when("/counseling-services", {
+            templateUrl: "js/counseling-services.component.html"
+        })
+        .when("/emdr", {
+            templateUrl: "js/emdr.component.html"
+        })
+        .when("/crisis-resources", {
+            templateUrl: "js/crisis-resources.component.html"
+        })
+        .when("/about", {
+            templateUrl: "js/about.component.html"
+        })
+        .when("/contact", {
+            templateUrl: "js/contact.component.html"
+        })
+        .otherwise("/");
+})
